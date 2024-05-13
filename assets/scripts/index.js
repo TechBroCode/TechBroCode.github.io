1e1936d2b998a04f0509f8bf6b6b980e45f7e368
TechBroCode <texhbro@gmail.com>
2024-05-13 09:30:00 +0100

    Modified file

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,35 +1,8 @@
 let width = document.querySelector("#width");
 let height = document.querySelector("#height");
-let cookieAccepted = false;
-let cookieArray = null, name = null, value = null;
-
-let cookies = {
-    readCookie: () => {
-        let allCookies = document.cookie;
-        document.write("All Cookies : " + allCookies);
-        cookieArray = allCookies.split(';');
-
-        for (let c = 0; c < cookieArray.length; c++) {
-            name = cookieArray[c].split('=')[0];
-            value = cookieArray[1].split('=')[1];
-            document.write("Key is : " + name + " and value is : " + value);
-        }
-        return name;
-    },
-    writeCookie: () => {
-
-    }
-}
 
 window.onload = () => {
     getWindowDimensions();
-    if (cookies.readCookie() !== null || cookies.readCookie() !== undefined) {
-//     Cookie is present then set cookieAccepted to true
-        cookieAccepted = true;
-    } else {
-        cookieAccepted = false;
-        document.querySelector(".cookieContainer").style.display =
-    }
 }
 
 function getWindowDimensions() {
