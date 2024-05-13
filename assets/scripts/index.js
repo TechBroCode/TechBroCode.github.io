102542e284ee0b0706829ffcb861a4d9e3b9b1b2
TechBroCode <texhbro@gmail.com>
2024-05-13 02:04:53 +0100

    Modified file

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,31 @@
 let width = document.querySelector("#width");
 let height = document.querySelector("#height");
-window.onload = () => getWindowDimensions();
+let cookieAccepted = false;
+let cookieArray = null, name = null, value = null;
+window.onload = () => {
+    getWindowDimensions();
+
+}
+
+let cookies = {
+    readCookie: () => {
+        let allCookies = document.cookie;
+        document.write("All Cookies : " + allCookies);
+        cookieArray = allCookies.split(';');
+
+        for (let c = 0; c < cookieArray.length; c++) {
+            name = cookieArray[c].split('=')[0];
+            value = cookieArray[1].split('=')[1];
+            document.write("Key is : " + name + " and value is : " + value);
+            name = null;
+            value = null;
+        }
+        return name;
+    },
+    writeCookie: () => {
+
+    }
+}
 
 function getWindowDimensions() {
     width.innerHTML = "width: " + window.innerWidth + " px";
