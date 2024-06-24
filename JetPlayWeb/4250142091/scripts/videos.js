73787e9769914e7f6eb6bf3a44141b56e763b37a
TechBroCode <texhbro@gmail.com>
2024-06-24 22:04:51 +0100

    Modified files

--- JetPlayWeb/4250142091/scripts/videos.js
+++ JetPlayWeb/4250142091/scripts/videos.js
@@ -52,6 +52,8 @@ fetch("https://techbrocode.github.io/JetPlayWeb/4250142091/assets/json/video.jso
                             iFrame.height = iFrame.contentWindow.document.body.scrollHeight + "px";
                             iFrame.style.height = iFrame.contentWindow.document.body.scrollHeight + "px";
                             iFrame.setAttribute("height", iFrame.contentWindow.document.body.scrollHeight + "px");
+                            allContent[0].removeChild(iFrame);
+                            allContent[0].appendChild(iFrame);
                             clearInterval(heightChecker);
                         }
                     }, 500);
