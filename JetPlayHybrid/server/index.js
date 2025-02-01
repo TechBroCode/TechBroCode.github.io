937d917988bb7dde3621ab118f00298c6ebf5428
TechBroCode <texhbro@gmail.com>
2025-02-02 00:50:20 +0100

    Modified files

---
+++ JetPlayHybrid/server/index.js
@@ -0,0 +1,118 @@
+let hasInternetConnection = null;
+const express = require("express");
+const path = require("path");
+require("dotenv").config({
+    encoding: "utf8", debug: false, override: false
+});
+const cors = require("cors");
+const networkPing = require("./scripts/network-ping.js");
+const http = require("http");
+const {Server} = require("socket.io");
+const app = express();
+let mainSocket = null;
+let networkPingCursor = 0;
+let mainNetworkCheckerInterval = null;
+const PORT = process !== undefined ? process.env !== undefined ? process.env.PORT !== undefined ? process.env.PORT : process.env.ALTERNATIVE_PORT : 3000 : 3000;
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        allowedOrigins: ["https://techbrocode.github.io"],
+        credentials: true,
+        methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]
+    }, connectionStateRecovery: {}
+});
+
+app.set("trust-proxy", true);
+app.use(cors({
+    allowedOrigins: ["https://techbrocode.github.io"],
+    credentials: true,
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]
+}));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+function checkNetworkRecursively() {
+    mainNetworkCheckerInterval = setInterval(async () => {
+        if (mainSocket !== undefined && mainSocket !== null) {
+            console.log("Ping url length => ", networkPing.length);
+            if (networkPingCursor === (networkPing.length)) {
+                networkPingCursor = 0;
+            }
+            let pingUrl;
+            try {
+                pingUrl = networkPing[networkPingCursor]?.url;
+            } catch (error) {
+                console.error(error);
+            } finally {
+                if (pingUrl === undefined) {
+                    pingUrl = "https://www.facebook.com";
+                    networkPingCursor = 0;
+                }
+                console.log(pingUrl);
+                try {
+                    const axios = require("axios");
+                    const response = await axios.head(pingUrl);
+                    if (response.status >= 200 && response.status < 300) {
+                        hasInternetConnection = true;
+                    } else {
+                        hasInternetConnection = false;
+                    }
+                    console.log("Network: ", response.status);
+                    if (mainSocket !== null && mainSocket !== undefined) {
+                        mainSocket.emit("networkResponse", {
+                            status: response.status,
+                            value: hasInternetConnection
+                        });
+                    }
+                } catch (e) {
+                    hasInternetConnection = false;
+                    console.error("Internet Error: ", e);
+                    if (mainSocket !== null && mainSocket !== undefined) {
+                        mainSocket.emit("networkResponse", {
+                            status: 500,
+                            value: hasInternetConnection
+                        });
+                    }
+                } finally {
+                    networkPingCursor++;
+                }
+            }
+        }
+    }, 2000);
+}
+
+io.on("connect", async socket => {
+    mainSocket = socket;
+    hasInternetConnection = false;
+    console.log(`User with id ${socket.id} connected...`);
+    socket.emit("networkResponse", {
+        status: undefined,
+        value: false
+    });
+
+    if (mainNetworkCheckerInterval === undefined || mainNetworkCheckerInterval === null) {
+        //checkNetworkRecursively();
+    }
+
+    socket.on("disconnect", async () => {
+        mainSocket = null;
+        hasInternetConnection = false;
+        console.log(`User disconnected with id ${socket.id}...`);
+        socket.emit("networkResponse", {
+            status: undefined,
+            value: false
+        });
+        if (mainNetworkCheckerInterval !== undefined && mainNetworkCheckerInterval !== null) {
+            clearInterval(mainNetworkCheckerInterval);
+            mainNetworkCheckerInterval = null;
+        }
+    });
+});
+
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+});
---
+++ JetPlayHybrid/server/scripts/network-ping.js
@@ -0,0 +1,31 @@
+const networkPing = [
+    {
+        name: "Facebook",
+        url: "https://www.facebook.com/",
+    },
+    /*{
+        name: "Netflix",
+        url: "https://www.netflix.com/",
+    },*/
+    {
+        name: "Cloudflare",
+        url: "https://www.cloudflare.com/",
+    },
+    {
+        name: "Apple",
+        url: "https://www.apple.com/",
+    },
+    {
+        name: "Microsoft",
+        url: "https://www.microsoft.com/",
+    },
+    /*{
+        name: "Twitter",
+        url: "https://www.twitter.com/",
+    },*/
+    {
+        name: "Github",
+        url: "https://www.github.com/",
+    }
+]
+module.exports = networkPing;
